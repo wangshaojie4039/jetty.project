d4ec70a689a0f9460e6e282d6b3080ec7f8c62db
Lachlan Roberts <lachlan@webtide.com>
2019-06-06 11:14:21 +1000

    Issue #3735 - test QuotedPrintable in multipart boundary
    Signed-off-by: Lachlan Roberts <lachlan@webtide.com>

--- jetty-util/src/test/java/org/eclipse/jetty/util/MultiPartInputStreamTest.java
+++ jetty-util/src/test/java/org/eclipse/jetty/util/MultiPartInputStreamTest.java
@@ -1075,10 +1075,37 @@ public class MultiPartInputStreamTest
         assertEquals(EnumSet.of(NonCompliance.QUOTED_PRINTABLE_TRANSFER_ENCODING), mpis.getNonComplianceWarnings());
     }
 
+    @Test
+    public void testQuotedPrintableInBoundary() throws Exception
+    {
+        String boundary = "--=_PartBoundary";
+        String contentType = "multipart/form-data, boundary="+boundary;
+        String contentWithEncodedPart =
+                "--"+boundary+"\r\n"+
+                        "Content-disposition: form-data; name=\"part1\"\r\n"+
+                        "Content-Transfer-Encoding: quoted-printable\r\n"+
+                        "Content-Type: text/plain\r\n"+
+                        "\r\n"+
+                        "truth=3Dbeauty" + "\r\n"+
+                        "--"+boundary+"--\r\n";
+        MultipartConfigElement config = new MultipartConfigElement(_dirname, 1024, 3072, 50);
+        MultiPartInputStreamParser mpis = new MultiPartInputStreamParser(new ByteArrayInputStream(contentWithEncodedPart.getBytes()),
+                contentType,
+                config,
+                _tmpDir);
+        mpis.setDeleteOnExit(true);
+        Collection<Part> parts = mpis.getParts();
+        assertEquals(1, parts.size());
 
+        Part p1 = mpis.getPart("part1");
+        assertNotNull(p1);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IO.copy(p1.getInputStream(), baos);
+        assertEquals("truth=beauty", baos.toString("US-ASCII"));
 
+        assertEquals(EnumSet.of(NonCompliance.QUOTED_PRINTABLE_TRANSFER_ENCODING), mpis.getNonComplianceWarnings());
+    }
 
-    
     private String createMultipartRequestString(String filename)
     {
         int length = filename.length();
